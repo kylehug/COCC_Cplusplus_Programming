bf1934d5410151f93fe137c48631c870229687cc
kylehug <klh360@gmail.com>
2017-03-19 12:30:46 -0700

    Added random asterisk and horizontal line. Still WIP.

--- Assignment3b/Assignment3b.cpp
+++ Assignment3b/Assignment3b.cpp
@@ -1,15 +1,34 @@
 // COCC C++ - Kyle Hug - Assignment 3b
-//  - Draw out a 2D Array ( WORK IN PROGRESS )
+//
+//  [x] Draw out a 2D Array of periods
+//	[x] Change a point to be an asterisk 
+//	[x] Draw a horizontal line
+//	[ ] Draw a vertical line ( WORK IN PROGRESS )
+//	[ ] Draw a diagonal line
+//	[x] Using Loops show the array on screen
+//
+//	[ ] Clean up warnings
+//
 
 #include <iostream>
 #include <string>
+#include <time.h>
 
 using namespace std;
 
+// Get a unique random integer
+int GetRandInt(int max, int seedOffset) {
+	
+	int randInt;
+
+	srand((unsigned)time(NULL) + seedOffset);
+	randInt = rand() % max;
+
+	return randInt;
+}
+
 int main()
 {
-	string userInput;
-	
 	// Initialize the 2D Array, "image"
 	const int X_SIZE = 70;
 	const int Y_SIZE = 20;
@@ -17,31 +36,82 @@ int main()
 	int x;
 	int y;
 
-	// Populate image with periods
-	for (x = 0; x < X_SIZE; x++) {
-		
-		for (y = 0; y < Y_SIZE; y++) {
-			
-			image[x][y] = '.';
-		}
-	}
+	// Initialize asterisk variables
+	int asterRandX;
+	int asterRandY;
 
-	// Print out the image
-	for (y = 0; y < Y_SIZE; y++) {
-		
-		for (x = 0; x < X_SIZE; x++) {
-			
-			cout << image[x][y];
-		}
-		
-		cout << endl;
-	}
+	// Initialize horizontal line variables
+	const int H_LINE_WIDTH = 10;
+	int hLineRandStart;
+	int hLineRandYPos;
+	
+	string userInput = "y";
 
 	// Pause at the end
-	while (userInput != "quit") {
+	while (userInput == "y") {
+		
+		// Clear user input
+		userInput = "";
 
-		cout << "Enter \"quit\" to end: ";
-		getline(cin, userInput);
+		// This is an ugly way to clear the screen, but at least it's OS independent...
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+		
+		// Set up population of the asterisk
+		asterRandX = GetRandInt(X_SIZE, 999);
+		asterRandY = GetRandInt(Y_SIZE, -69);
+	
+		// Set up population of the horizontal line
+		hLineRandStart = GetRandInt(X_SIZE, 42);
+		hLineRandYPos = GetRandInt(Y_SIZE, 13);
+		
+		// Populate image
+		for (x = 0; x < X_SIZE; x++) {
+		
+			for (y = 0; y < Y_SIZE; y++) {
+			
+				// Populate a random asterisk
+				if ((x == asterRandX) && y == (asterRandY)) {
+				
+					image[x][y] = '*';
+				}
+				// Populate the horizontal line
+				else if ((x >= hLineRandStart) && (x <= (hLineRandStart + H_LINE_WIDTH)) && (y == hLineRandYPos)) {
+
+					image[x][y] = '-';
+
+				}
+			
+				// Populate with a period
+				else {
+				
+					image[x][y] = '.';
+				}
+			}
+		}
+
+		// Print out the image
+		for (y = 0; y < Y_SIZE; y++) {
+		
+			// Offset image position on screen
+			cout << '\t';
+
+			for (x = 0; x < X_SIZE; x++) {
+			
+				cout << image[x][y];
+			}
+		
+			cout << endl;
+		}
+
+		// Spacer
+		cout << "\n\n";
+
+		// Ask to quit or start over
+		while ((userInput != "y") && (userInput != "n")) {
+
+			cout << "Draw Again? [y/n]: ";
+			getline(cin, userInput);
+		}
 	}
 
 	return 0;
