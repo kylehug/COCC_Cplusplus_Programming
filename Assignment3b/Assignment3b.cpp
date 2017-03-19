fa35221c58a2980f5071161459a1760b4085823d
kylehug <klh360@gmail.com>
2017-03-19 10:29:23 -0700

    Started assignment and made functional. Currently all it does is initialize the 2D array with periods and prints it out.

--- Assignment3b/Assignment3b.cpp
+++ Assignment3b/Assignment3b.cpp
@@ -1,11 +1,49 @@
-// Assignment3b.cpp : Defines the entry point for the console application.
-//
+// COCC C++ - Kyle Hug - Assignment 3b
+//  - Draw out a 2D Array ( WORK IN PROGRESS )
 
-#include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+	string userInput;
+	
+	// Initialize the 2D Array, "image"
+	const int X_SIZE = 70;
+	const int Y_SIZE = 20;
+	char image[X_SIZE][Y_SIZE];
+	int x;
+	int y;
+
+	// Populate image with periods
+	for (x = 0; x < X_SIZE; x++) {
+		
+		for (y = 0; y < Y_SIZE; y++) {
+			
+			image[x][y] = '.';
+		}
+	}
+
+	// Print out the image
+	for (y = 0; y < Y_SIZE; y++) {
+		
+		for (x = 0; x < X_SIZE; x++) {
+			
+			cout << image[x][y];
+		}
+		
+		cout << endl;
+	}
+
+	// Pause at the end
+	while (userInput != "quit") {
+
+		cout << "Enter \"quit\" to end: ";
+		getline(cin, userInput);
+	}
+
+	return 0;
 }
 
