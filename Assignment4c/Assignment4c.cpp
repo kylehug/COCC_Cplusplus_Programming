bee93f60ee9f069056710c04b7d66ffc411c1094
kylehug <klh360@gmail.com>
2017-03-25 16:03:05 -0700

    Started Assignment 4c.

---
+++ Assignment4c/Assignment4c.cpp
@@ -0,0 +1,29 @@
+// Assignment4c.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Function Declarations
+void drawFrame(vector<int>& image); // Redraws the image on the screen
+
+// Main function
+int main() {
+
+	string userPause;
+
+	cout << "Image will go here";
+	cin >> userPause;
+	
+	return 0;
+}
+
+// Redraws the image on the screen
+void drawFrame(vector<int>& image) {
+
+	return;
+}
