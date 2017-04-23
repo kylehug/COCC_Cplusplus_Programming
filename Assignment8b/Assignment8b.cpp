e432dcdaca0a616f132b61ab860adf89cfb8055b
kylehug <klh360@gmail.com>
2017-04-23 14:29:35 -0700

    Getting started...

---
+++ Assignment8b/Assignment8b.cpp
@@ -0,0 +1,13 @@
+/**
+COCC C++ - Assignment 8B - Kyle Hug
+
+GOALS:
+[ ] Write a function add() that takes 4 parameters of arbitrary types and adds them together...
+
+*/
+
+int main()
+{
+    return 0;
+}
+
