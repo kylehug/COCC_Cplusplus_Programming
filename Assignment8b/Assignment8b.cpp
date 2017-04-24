01d724a29f0af2318abb630102b24ba668b066bd
kylehug <klh360@gmail.com>
2017-04-23 22:16:14 -0700

    Assignment Submission

--- Assignment8b/Assignment8b.cpp
+++ Assignment8b/Assignment8b.cpp
@@ -6,8 +6,44 @@ GOALS:
 
 */
 
-int main()
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename typeA, typename typeB, typename typeC, typename typeD>
+typeA add(typeA numA, typeB numB, typeC numC, typeD numD)
 {
-    return 0;
+	return numA + numB + numC + numD;
 }
 
+
+int main()
+{
+	string userInput;
+
+	cout << "\nExample with floats:\n\tadd(1.0, 2.0, 3.0, 4.0) = ";
+	cout << add(1.0, 2.0, 3.0, 4.0) << endl;
+
+	cout << "\nExample with ints:\n\tadd(1, 2, 3, 4) = ";
+	cout << add(1, 2, 3, 4) << endl;
+
+	cout << "\nExample with mixed number types:\n\tadd(1, 2, 3.0, 4.0) = ";
+	cout << add(1, 2, 3.0, 4.0) << endl;
+
+	cout << "\nExample with chars:\n\tadd('1', '2', '3', '4') = ";
+	cout << add('1', '2', '3', '4') << endl;
+	cout << "\n\tI think we get this result because it's adding the char ASCII values, resulting in a character at the index of the added value.\n";
+
+	cout << "\nExample with strings:\n\tadd(\"A\", \"B\", \"C\", \"D\") = ";
+	cout << add(string("A"), string("B"), string("C"), string("D")) << endl;
+	cout << "\n\tI think we get this result because the string type has overridden the \"+\" operator to append strings, rather than add their character values.\n";
+
+	cout << "\nExample with mixed ints/string:\n\tadd(\"A\", 2, 3, 4) = FAILS\n";
+	//cout << add(string("A"), 2, 3, 4) << endl;
+	cout << "\n\tI think this fails because the compiler gets confused between the two separate behaviors of the \"+\" operator with numerical types and strting types.";
+
+	cout << "Any key to quit\n> ";
+	getline(cin, userInput);
+
+	return 0;
+}
